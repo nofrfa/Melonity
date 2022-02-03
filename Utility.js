ff7aa6a26ec39a214159884724455effd142364e
nofrfa <72886464+nofrfa@users.noreply.github.com>
2022-02-03 21:45:02 +0300

    update: Utility [release-1.0.5]

--- Utility.js
+++ Utility.js
@@ -91,31 +91,48 @@
   !*** ./src/Utility.ts ***!
   \************************/
 /*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
-
-Object.defineProperty(exports, "__esModule", { value: true });
-const lib_1 = __webpack_require__(/*! ./libs/lib */ "./src/libs/lib.ts");
 let CustomUtility = {};
+function GetTipStringImage(imagePath) {
+    return '{{' + imagePath + ':false}}';
+}
+function GetImagePath(abilityname) {
+    return 'panorama/images/' + abilityname + '_png.vtex_c';
+}
 var customUtil;
 (function (customUtil) {
     let myHero, myPlayer, gameStarted;
     const path = ['Custom Scripts', 'Other'];
+    //Roshan FuckPos
     const Poses = [
         new Vector(-2246, 2215, 13),
         new Vector(-2404, 2033, 28),
         new Vector(-2528, 1888, 26)
     ];
     const image = Renderer.LoadImage(`panorama/images/hud/reborn/roshan_timer_roshan_psd.vtex_c`);
-    let menuEnable = Menu.AddToggle([...path, 'Roshan InvisPos'], 'Enable', false)
+    let roshENABLE = Menu.AddToggle([...path, 'Roshan InvisPos'], 'Enable', false)
         .SetNameLocale('ru', 'Включить')
-        .SetTip(`Adds buttons: ${lib_1.GetTipStringImage('panorama/images/hud/reborn/roshan_timer_roshan_psd.vtex_c')}, at the entrance to Roshan pit buttons, clicking on which, your hero will stand in a position where he can be seen only when he is nearby`, 'en')
-        .SetTip(`Добавляет кнопки: ${lib_1.GetTipStringImage('panorama/images/hud/reborn/roshan_timer_roshan_psd.vtex_c')}, у входа в логово Рошана кнопки, нажав на которые, ваш герой встанет в позицию, когда его можно будет увидеть только оказавшись рядом`, 'ru')
+        .SetTip(`Adds buttons: ${GetTipStringImage('panorama/images/hud/reborn/roshan_timer_roshan_psd.vtex_c')}, at the entrance to Roshan pit buttons, clicking on which, your hero will stand in a position where he can be seen only when he is nearby`, 'en')
+        .SetTip(`Добавляет кнопки: ${GetTipStringImage('panorama/images/hud/reborn/roshan_timer_roshan_psd.vtex_c')}, у входа в логово Рошана кнопки, нажав на которые, ваш герой встанет в позицию, когда его можно будет увидеть только оказавшись рядом`, 'ru')
         .OnChange(state => {
-        menuEnable = state.newValue;
+        roshENABLE = state.newValue;
     })
         .GetValue();
+    Menu.GetFolder([...path, 'Roshan InvisPos']).SetImage('panorama/images/hud/reborn/roshan_timer_roshan_psd.vtex_c');
+    //Courier
+    const shopPos = new Vector(4893, -1193, 128);
+    const bestPos = new Vector(5253, -1083, 256);
+    let cour;
+    let courierENABLE = Menu.AddToggle([...path, 'Courier'], 'Safe Pos', false)
+        .SetNameLocale('ru', 'Безопасная позиция')
+        .SetTip('If the courier is sent to a secret shop on the side of the forces of dire\nThe courier will get into a safer position', 'en')
+        .SetTip('Если курьер будет отправлен в потайную лавку на стороне сил тьмы\nКурьер встанет в более безопасную позицию', 'ru')
+        .OnChange(state => {
+        courierENABLE = state.newValue;
+    })
+        .GetValue();
+    Menu.GetFolder([...path, 'Courier']).SetImage(GetImagePath('items/courier'));
     CustomUtility.OnScriptLoad = CustomUtility.OnGameStart = () => {
         if (GameRules.IsActiveGame()) {
             myHero = EntitySystem.GetLocalHero();
@@ -123,17 +140,21 @@ var customUtil;
             gameStarted = true;
         }
         if (!myHero || !myHero.IsExist() || myHero.GetUnitName() == null) {
+            myHero = null;
+            myPlayer = null;
             gameStarted = false;
-            return;
+            cour = null;
         }
     };
     CustomUtility.OnGameEnd = () => {
         myHero = null;
         myPlayer = null;
         gameStarted = false;
+        cour = null;
     };
     CustomUtility.OnDraw = () => {
-        if (gameStarted && menuEnable) {
+        if (gameStarted && roshENABLE && !Engine.IsShopOpen()) {
+            //rosh
             for (let pos of Poses) {
                 let [x, y, OnScreen] = Renderer.WorldToScreen(pos);
                 if (!OnScreen)
@@ -153,215 +174,61 @@ var customUtil;
             }
         }
     };
+    function Dist2D(vec1, vec2) {
+        if (vec1 && vec2) {
+            let pos1 = (vec1.x ? (vec1) : (vec1.GetAbsOrigin ? (vec1.GetAbsOrigin()) : (0)));
+            let pos2 = (vec2.x ? (vec2) : (vec2.GetAbsOrigin ? (vec2.GetAbsOrigin()) : (0)));
+            return pos1 && pos2 && pos1.sub(pos2).Length2D();
+        }
+        return -1;
+    }
+    function FindCour() {
+        if (!myHero)
+            return;
+        let couriers_list = EntitySystem.GetCouriersList();
+        for (let couri of couriers_list) {
+            if (couri.GetProperty('C_DOTA_Unit_Courier', 'm_nSoleControllingPlayer') == myPlayer.GetPlayerID()) {
+                cour = couri;
+                break;
+            }
+        }
+    }
     CustomUtility.OnUpdate = () => {
-        if (gameStarted && menuEnable) {
-            for (let pos of Poses) {
-                let [x, y, OnScreen] = Renderer.WorldToScreen(pos);
-                if (!OnScreen)
-                    continue;
-                if (Input.IsCursorInRect(x, y, 32, 32, Enum.ContentAlign.CenterXY) && Input.IsKeyDownOnce(Enum.ButtonCode.MOUSE_LEFT)) {
-                    myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION, null, pos, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, myHero, false, true);
+        if (gameStarted) {
+            if (roshENABLE && !Engine.IsShopOpen()) {
+                //rosh
+                for (let pos of Poses) {
+                    let [x, y, OnScreen] = Renderer.WorldToScreen(pos);
+                    if (!OnScreen)
+                        continue;
+                    if (Input.IsCursorInRect(x, y, 32, 32, Enum.ContentAlign.CenterXY) && Input.IsKeyDownOnce(Enum.ButtonCode.MOUSE_LEFT)) {
+                        myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION, null, pos, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, myHero, false, true);
+                    }
+                }
+            }
+            if (courierENABLE && myHero.GetTeamNum() == 3) {
+                if (Engine.OnceAt(1)) {
+                    if (!cour || !cour.IsExist()) {
+                        FindCour();
+                        return;
+                    }
+                    if (cour.IsAlive()) {
+                        let cour_state = cour.GetCourierState();
+                        if (cour_state == 0 && Dist2D(shopPos, cour) <= 700 && Dist2D(bestPos, cour) >= 150) {
+                            myPlayer.PrepareUnitOrdersStructed({
+                                position: bestPos,
+                                orderType: Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION,
+                                entity: cour,
+                                forcedType: (Renderer.WorldToScreen(bestPos)[2] ? Enum.ForcedType.None : Enum.ForcedType.Minimap)
+                            });
+                        }
+                    }
                 }
             }
         }
     };
     RegisterScript(CustomUtility);
 })(customUtil || (customUtil = {}));
-
-
-/***/ }),
-
-/***/ "./src/libs/lib.ts":
-/*!*************************!*\
-  !*** ./src/libs/lib.ts ***!
-  \*************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RADIAN_TO_PI_KOEFF = 180 / Math.PI;
-function GetImagesPath(name, full) {
-    if (name.startsWith('item_')) {
-        return `panorama/images/items/${name.slice(5)}_png.vtex_c`;
-    }
-    else if (name.startsWith('npc_dota_hero')) {
-        if (full) {
-            return `panorama/images/heroes/${name}_png.vtex_c`;
-        }
-        else {
-            return `panorama/images/heroes/icons/${name}_png.vtex_c`;
-        }
-    }
-    else if (name.startsWith('npc_dota_neutral')) {
-        return `panorama/images/heroes/${name}_png.vtex_c`;
-    }
-    else {
-        return `panorama/images/spellicons/${name}_png.vtex_c`;
-    }
-}
-exports.GetImagesPath = GetImagesPath;
-function GetTipStringImage(imagePath) {
-    return '{{' + imagePath + ':false}}';
-}
-exports.GetTipStringImage = GetTipStringImage;
-function CreateMultiSelect(path, name, iconsArray, default_value = true, translate) {
-    let icons = [];
-    for (let q of iconsArray) {
-        icons.push(GetImagesPath(q));
-    }
-    let a = Menu.AddMultiSelect(path, name, icons, default_value);
-    if (translate) {
-        if (typeof translate === 'string') {
-            a.SetNameLocale('ru', translate);
-        }
-        else {
-            for (let q of translate) {
-                // struct: array {"language", "translate"}
-                a.SetNameLocale(q.language, q['translate']);
-            }
-        }
-    }
-    return {
-        GetOption: () => {
-            return a;
-        },
-        IsEnabled: (name) => {
-            let n = name;
-            if (typeof name === 'object') {
-                if (name.GetEntityName()) {
-                    n = name.GetEntityName();
-                }
-                if (name.GetName()) {
-                    n = name.GetName();
-                }
-            }
-            return a.GetValue()[iconsArray.indexOf(n)];
-        }
-    };
-}
-exports.CreateMultiSelect = CreateMultiSelect;
-function CreatePrioritySelect(path, name, iconsArray, default_value = true, translate) {
-    let icons = [];
-    for (let q of iconsArray) {
-        icons.push(GetImagesPath(q));
-    }
-    let a = Menu.AddPrioritySelect(path, name, icons, default_value);
-    if (translate) {
-        if (typeof translate === 'string') {
-            a.SetNameLocale('ru', translate);
-        }
-        else {
-            for (let q of translate) {
-                // struct: array {"language", "translate"}
-                a.SetNameLocale(q.language, q['translate']);
-            }
-        }
-    }
-    return {
-        GetOption: () => {
-            return a;
-        },
-        GetValue: () => {
-            let t = [];
-            for (let e of a.GetValue()) {
-                t.push(iconsArray[e]);
-            }
-            return t;
-        }
-    };
-}
-exports.CreatePrioritySelect = CreatePrioritySelect;
-function IsntUndefined(value, withfalse) {
-    return withfalse ? (value !== false) : value !== undefined && value !== null;
-}
-function IsUndefined(value, withfalse) {
-    return withfalse ? (value === false || !value) : (value !== false && !value);
-}
-exports.IsUndefined = IsUndefined;
-function Dist2D(vec1, vec2) {
-    if (vec1 && vec2) {
-        let pos1 = (vec1.x ? (vec1) : (vec1.GetAbsOrigin ? (vec1.GetAbsOrigin()) : (0)));
-        let pos2 = (vec2.x ? (vec2) : (vec2.GetAbsOrigin ? (vec2.GetAbsOrigin()) : (0)));
-        return pos1 && pos2 && pos1.sub(pos2).Length2D();
-    }
-    return -1;
-}
-exports.Dist2D = Dist2D;
-function GetAngleToPos(_e1, _e2, prefer = _e2, inrad) {
-    let [a, b] = [IsntUndefined(_e1.x) ? _e1 : _e1.GetAbsOrigin(), IsntUndefined(_e2.x) ? _e2 : _e2.GetAbsOrigin()];
-    if (prefer == _e1) {
-        [a, b] = [b, a];
-    }
-    let atan2 = Math.atan2(b.y - a.y, b.x - a.x);
-    return inrad ? atan2 : (atan2 * exports.RADIAN_TO_PI_KOEFF);
-}
-exports.GetAngleToPos = GetAngleToPos;
-function GetBestPosition(units, MainEntity, range) {
-    if (!units || !range) {
-        return;
-    }
-    if (!(units.length > 0)) {
-        return;
-    }
-    let enemyNum = units.length;
-    if (enemyNum == 1) {
-        return units[0].GetAbsOrigin();
-    }
-    let maxNum = 1;
-    let bestPos = units[0].GetAbsOrigin();
-    for (let i = 0; i < (enemyNum - 1); i++) {
-        for (let j = i + 1; j < (enemyNum); j++) {
-            if (units[i] && units[j]) {
-                let pos1 = units[i].GetAbsOrigin();
-                let pos2 = units[j].GetAbsOrigin();
-                let mid = pos1.add(pos2).Scaled(0.5);
-                let heroesNum = 0;
-                for (let k = 0; k < enemyNum; k++) {
-                    if (units[k].IsPositionInRange(mid, range, 0) && units[k].IsEntityInRange(MainEntity, range) && !units[k].IsDormant()) {
-                        heroesNum = heroesNum + 1;
-                    }
-                }
-                if (heroesNum > maxNum) {
-                    maxNum = heroesNum;
-                    bestPos = mid;
-                }
-            }
-        }
-    }
-    return bestPos;
-}
-exports.GetBestPosition = GetBestPosition;
-function GetHeroInRadiusWithPosition(Position, range, myHero, IsEnemy = true) {
-    let hero_list = EntitySystem.GetHeroesList();
-    let returnheroes = [];
-    if (hero_list) {
-        for (let i of hero_list) {
-            if (i && !i.IsIllusion() && !i.IsMeepoClone() && i.IsHero() && i.IsAlive() && !i.IsDormant() && ((!i.IsSameTeam(myHero) && IsEnemy) || (IsEnemy === false && i.IsSameTeam(myHero))) && i.IsPositionInRange(Position, range, 0)) {
-                returnheroes.push(i);
-            }
-        }
-        return returnheroes;
-    }
-    return [];
-}
-exports.GetHeroInRadiusWithPosition = GetHeroInRadiusWithPosition;
-function CustomCanCast(item) {
-    let owner = item.GetOwner(), hasModf = owner.HasState(Enum.ModifierState.MODIFIER_STATE_MUTED)
-        || owner.HasState(Enum.ModifierState.MODIFIER_STATE_STUNNED)
-        || owner.HasState(Enum.ModifierState.MODIFIER_STATE_HEXED)
-        || owner.HasState(Enum.ModifierState.MODIFIER_STATE_INVULNERABLE)
-        || owner.HasState(Enum.ModifierState.MODIFIER_STATE_FROZEN)
-        || owner.HasState(Enum.ModifierState.MODIFIER_STATE_FEARED)
-        || owner.HasState(Enum.ModifierState.MODIFIER_STATE_TAUNTED);
-    return item && !hasModf && owner.GetMana() >= item.GetManaCost() && item.IsCastable(owner.GetMana());
-}
-exports.CustomCanCast = CustomCanCast;
-function SendOrderMovePos(vector, myHero, myPlayer) {
-    myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION, null, vector, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, myHero, false, true);
-}
-exports.SendOrderMovePos = SendOrderMovePos;
 
 
 /***/ }),
@@ -373,7 +240,7 @@ exports.SendOrderMovePos = SendOrderMovePos;
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\MayTo\AppData\Roaming\Minority\scripts\src\Utility.ts */"./src/Utility.ts");
+module.exports = __webpack_require__(/*! C:\Users\MayTo\AppData\Roaming\Melonity\scripts\src\Utility.ts */"./src/Utility.ts");
 
 
 /***/ })
